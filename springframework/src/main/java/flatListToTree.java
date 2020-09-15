22460f89bb21d4c754c3c44df16a093d7dc7cdda
Valery Bulekov <muzaccord@gmail.com>
2020-09-15 21:00:15 +0300

    Переработка дерева.

---
+++ springframework/src/main/java/flatListToTree.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class flatListToTree {
+    static class Node {
+        private Integer nodeNumber;
+
+        private List<Node> childNodes;
+
+        private Integer parentNodeNumber;
+
+        public Node(Integer nodeNumber, Integer parentNodeNumber) {
+            this.nodeNumber = nodeNumber;
+            this.parentNodeNumber = parentNodeNumber;
+            this.childNodes = new ArrayList<>();
+        }
+
+        public void printChildren() {
+            if (childNodes.size() > 0) {
+                System.out.println("\nChildren of node #" + nodeNumber);
+                System.out.print("\t");
+                for (Node childNode : childNodes) {
+                    System.out.print(childNode.nodeNumber + " ");
+                }
+            }
+        }
+
+        public void addChild(Node node) {
+            childNodes.add(node);
+        }
+
+        public Integer getNodeNumber() {
+            return nodeNumber;
+        }
+
+        public List<Node> getChildNodes() {
+            return childNodes;
+        }
+
+        public Integer getParentNodeNumber() {
+            return parentNodeNumber;
+        }
+    }
+
+    static List<Node> flatList;
+
+    public static void main(String[] args) {
+        flatList = new ArrayList<>();
+        flatList.add(new Node(1, null));
+        flatList.add(new Node(2, null));
+        flatList.add(new Node(3, 1));
+        flatList.add(new Node(4, 3));
+        flatList.add(new Node(5, null));
+        flatList.add(new Node(6, 3));
+        flatList.add(new Node(7, 2));
+        flatList.add(new Node(8, 5));
+
+        List<Node> tree = new ArrayList<>(flatList);
+
+        List<Integer> toRemove = new ArrayList<>();
+        for (Node flatNode : flatList) {
+            Integer nodeParentNumber = flatNode.getParentNodeNumber();
+            for (Node treeNode : tree) {
+                if (treeNode.getNodeNumber().equals(nodeParentNumber)) {
+                    treeNode.addChild(flatNode);
+                    toRemove.add(tree.indexOf(treeNode));
+                }
+            }
+        }
+
+        for (Integer index : toRemove) {
+            tree.remove(index);
+        }
+        tree.get(1).printChildren();
+        tree.get(2).printChildren();
+    }
+}
